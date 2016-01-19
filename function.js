0c219faecccbca0aad81b7ce3264a2505063411c
MogilevichDmitry <dmsmain@gmail.com>
2016-01-19 14:57:22 +0300

    v0.2

--- function.js
+++ function.js
@@ -2,7 +2,7 @@
  * Created by Дмитрий on 12.01.2016.
  */
 //document.addEventListener("DOMContentLoaded", calcRoots);
-function calcRoots()
+function calcRoots(container)
 {
     var a = document.getElementById("a").value;
     var b = document.getElementById("b").value;
@@ -14,22 +14,32 @@ function calcRoots()
         x1 = x2 = -b / (2 * a);
         document.getElementById("x1").value = x1;
         document.getElementById("x2").value = x2;
+        resetMsg(container);
     }
     else if (D > 0){
         x1 = (-b - Math.sqrt(D)) / (2 * a);
         x2 = (-b + Math.sqrt(D)) / (2 * a);
         document.getElementById("x1").value = x1;
         document.getElementById("x2").value = x2;
+        resetMsg(container);
     }
     else {
-       // alert(D + " отрицательный дискрименант");
+        resetMsg(container);
         var tempD, tempX;
         tempD = Math.sqrt(-D) / ( 2 * a);
         tempD = tempD.toString();
         tempX = -b / (2 * a);
         tempX = tempX.toString();
-        document.getElementById("x1").value = + tempX +"+"+ tempD + "i";
+        document.getElementById("x1").value = + tempX + "+" + tempD + "i";
         document.getElementById("x2").value = + tempX + "-" + tempD + "i";
+        var msgComplex = document.createElement('span');
+        msgComplex.className = "msgComplexNumbers";
+        msgComplex.innerHTML = "Корни существуют только для комплексных чисел";
+        container.appendChild(msgComplex);
     }
 }
-
+function resetMsg(container){
+    if(container.lastChild.className =="msgComplexNumbers"){
+        container.removeChild(container.lastChild);
+    }
+}
